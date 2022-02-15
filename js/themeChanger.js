8cc58fb04fc6f232d614849a6f7d8b7495fba385
lokot0k <kaptoxa322@mail.ru>
2022-02-15 22:22:01 +0300

    code polished

--- js/themeChanger.js
+++ js/themeChanger.js
@@ -8,30 +8,14 @@ const themes = {
         '--background-image-var': 'url("/assets/background-light.jpg")'
     },
     'dark': {
-        '--background-color-var': '#0d1117',
-        '--background-opacity-var': 'rgba(0,0,0, .13)',
         '--shadow-color-var': '#0d1117',
         '--border-color-var': 'aliceblue',
+        '--background-color-var': '#0d1117',
         '--text-color-var': 'white',
+        '--background-opacity-var': 'rgba(0,0,0, .13)',
         '--background-image-var': 'url("/assets/background-dark.png")'
     }
 };
-
-const themeSelector = document.getElementById("theme");
-// setting key in localStorage for correct theme change
-let oldTheme = localStorage.getItem("my_page_theme");
-if (oldTheme === null) {
-    oldTheme = themeSelector.value;
-    localStorage.setItem("my_page_theme", themeSelector.value);
-}
-themeSelector.addEventListener("change", changeTheme);
-themeSelector.value = oldTheme;
-themeSelector.dispatchEvent(new Event("change")); // setting the theme from localStorage
-
-
-// calculating and setting my current age
-const difference = new Date() - new Date("2004/03/07");
-document.getElementById("current-age").innerText = Math.floor(difference / 1000 / 60 / 24 / 365 / 60).toString();
 
 function changeTheme() {
     const currentTheme = themeSelector.value;
@@ -42,3 +26,17 @@ function changeTheme() {
         htmlThemes.style.setProperty(key, themeSettings[key]);
     });
 }
+
+const themeSelector = document.getElementById("theme");
+
+// setting key in localStorage for correct theme change
+let oldTheme = localStorage.getItem("my_page_theme");
+if (oldTheme === null) {
+    oldTheme = themeSelector.value;
+    localStorage.setItem("my_page_theme", themeSelector.value);
+}
+
+// setting the theme from localStorage
+themeSelector.addEventListener("change", changeTheme);
+themeSelector.value = oldTheme;
+themeSelector.dispatchEvent(new Event("change"));
